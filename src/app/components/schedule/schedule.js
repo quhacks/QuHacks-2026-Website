9b99a2e278c284ea4c77bcd07e76968af7462369
devsai9 <63507117+devsai9@users.noreply.github.com>
2026-02-07 00:58:05 -0500

    Update schedule rooms

--- src/app/components/schedule/schedule.js
+++ src/app/components/schedule/schedule.js
@@ -11,9 +11,8 @@ export default function ScheduleSection() {
                 <thead>
                     <tr>
                         <th className={styles.th}>Time</th>
-                        <th className={styles.th}>AUD</th>
-                        <th className={styles.th}>R1 TBD</th>
-                        <th className={styles.th}>R2 TBD</th>
+                        <th className={styles.th}>DH 100</th>
+                        <th className={styles.th}>DH 108</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -21,23 +20,19 @@ export default function ScheduleSection() {
                         <td className={styles.td}>9:00 AM - 10:00 AM</td>
                         <td className={styles.td}>Check-In</td>
                         <td className={styles.td}></td>
-                        <td className={styles.td}></td>
                     </tr>
                     <tr>
                         <td className={styles.td}>10:00 AM - 10:15 AM</td>
                         <td className={styles.td}>Opening Ceremony</td>
                         <td className={styles.td}></td>
-                        <td className={styles.td}></td>
                     </tr>
                     <tr>
                         <td className={styles.td}>10:15 AM</td>
                         <td className={styles.td}>Hacking Begins!</td>
                         <td className={styles.td}>Hacking Begins!</td>
-                        <td className={styles.td}>Hacking Begins!</td>
                     </tr>
                     <tr>
                         <td className={styles.td}>10:15 AM</td>
-                        <td className={styles.td}>Team Formation</td>
                         <td className={styles.td}>Team Formation</td>
                         <td className={styles.td}>Team Formation</td>
                     </tr>
@@ -45,41 +40,34 @@ export default function ScheduleSection() {
                         <td className={styles.td}>10:30 AM - 11:30 AM</td>
                         <td className={styles.td}></td>
                         <td className={styles.td}>Intro to Python</td>
-                        <td className={styles.td}></td>
                     </tr>
                     <tr>
                         <td className={styles.td}>11:45 AM - 12:30 PM</td>
                         <td className={styles.td}></td>
                         <td className={styles.td}>Solona Workshop</td>
-                        <td className={styles.td}></td>
                     </tr>
                     <tr>
                         <td className={styles.td}>1:00 PM - 2:00 PM</td>
                         <td className={styles.td}>Lunch</td>
-                        <td className={styles.td}></td>
                         <td className={styles.td}></td>
                     </tr>
                     <tr>
                         <td className={styles.td}>2:00 PM - 3:00 PM</td>
                         <td className={styles.td}></td>
                         <td className={styles.td}>Getting Started with Web Dev</td>
-                        <td className={styles.td}></td>
                     </tr>
                     <tr>
                     <td className={styles.td}>3:15 PM - 3:45 PM</td>
                         <td className={styles.td}></td>
                         <td className={styles.td}>Intro to AI</td>
-                        <td className={styles.td}></td>
                     </tr>
                     <tr>
                     <td className={styles.td}>4:00 PM - 4:30 PM</td>
                         <td className={styles.td}></td>
                         <td className={styles.td}>Monkey Type Competition</td>
-                        <td className={styles.td}></td>
                     </tr>
                     <tr>
                         <td className={styles.td}>4:45 PM</td>
-                        <td className={styles.td}>Hacking Ends!</td>
                         <td className={styles.td}>Hacking Ends!</td>
                         <td className={styles.td}>Hacking Ends!</td>
                     </tr>
@@ -87,12 +75,10 @@ export default function ScheduleSection() {
                         <td className={styles.td}>4:45 PM - 5:45 PM</td>
                         <td className={styles.td}>Judging</td>
                         <td className={styles.td}>Judging</td>
-                        <td className={styles.td}>Judging</td>
                     </tr>
                     <tr>
                         <td className={styles.td}>5:45 PM - 6:00 PM</td>
                         <td className={styles.td}>Closing Ceremony</td>
-                        <td className={styles.td}></td>
                         <td className={styles.td}></td>
                     </tr>
                 </tbody>
