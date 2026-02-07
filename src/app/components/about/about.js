fd92fbc76ac467171d63f43dad7d343f5cc470a1
devsai9 <63507117+devsai9@users.noreply.github.com>
2026-02-07 01:09:10 -0500

    Minor style changes to match the theme

--- src/app/components/about/about.js
+++ src/app/components/about/about.js
@@ -7,8 +7,8 @@ export default function AboutSection() {
             <div className={styles.aboutInfoContainer}>
                 <h1 className={styles.title}>About Us</h1>
 
-                <p className={styles.text}>QuHacks is a day-long hackathon  <font color="#ac28d4">by students, for students.</font> Join us for a day of <font color="#ac28d4">creation, innovation, and fun</font> with fellow middle and high schoolers interested in coding. From seasoned coding veterans to first-time programmers,  <font color="#ac28d4">all are welcome to join</font>.
-                    Discover your ability to create change by developing technology to solve real-world problems, attend workshops, and meet some new friends. <font color="#ac28d4">We can't wait to see you there!</font>
+                <p className={styles.text}>QuHacks is a day-long hackathon  <font color="#2866d2">by students, for students.</font> Join us for a day of <font color="#2866d2">creation, innovation, and fun</font> with fellow middle and high schoolers interested in coding. From seasoned coding veterans to first-time programmers,  <font color="#2866d2">all are welcome to join</font>.
+                    Discover your ability to create change by developing technology to solve real-world problems, attend workshops, and meet some new friends. <font color="#2866d2">We can't wait to see you there!</font>
                 </p>
                 <Link className={styles.learnMore} href="/timeline"> Learn more... </Link>
             </div>
