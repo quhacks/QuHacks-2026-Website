701fd5ccd41e78ce92d6680a792df02d5f9d490d
Ankit Mohanty <ankitvmohanty5@gmail.com>
2026-09-06 23:05:50 -0400

    Update team card typography

--- src/app/components/team/teamcard/teamcard.js
+++ src/app/components/team/teamcard/teamcard.js
@@ -1,8 +1,21 @@
 import styles from './component.module.css'
+import { IBM_Plex_Mono, Space_Grotesk } from 'next/font/google'
+
+const teamNameFont = Space_Grotesk({
+    subsets: ['latin'],
+    weight: ['600', '700'],
+    variable: '--font-team-name',
+})
+
+const teamPositionFont = IBM_Plex_Mono({
+    subsets: ['latin'],
+    weight: ['400'],
+    variable: '--font-team-position',
+})
 
 export default function TeamCard(props) {
     return (
-        <div className={styles.card}>
+        <div className={`${styles.card} ${teamNameFont.variable} ${teamPositionFont.variable}`}>
             <div className={styles.imageContainer}>
                 <img
                     className={styles.image}
